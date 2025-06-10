5f70ba4ab65ff2526985dd7d78e96df95361a28a
Kenrikk <faelnar.kenrikk@gmail.com>
2025-06-10 14:28:08 +0800

    Created function for button to reveal secret message

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+    const message = document.getElementById("message");
+    message.innerHTML = "Thank you for always being my rock, my guide, and my greatest supporter. Your love and wisdom have shaped me into who I am today. I am forever grateful for your presence in my life. I love you, Dad! Happy Father's Day! From LNDK";
+}
